bc8483a354e621db164e1001b9c40670f68f6c75
gaffarovaAlbina <gaffarova_albina@mail.ru>
2022-05-06 14:54:40 +0300

    make it better

--- config/settings.py
+++ config/settings.py
@@ -151,12 +151,6 @@ USE_TZ = False
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = "static/"
 
-# The absolute path to the directory where collectstatic will collect static files for deployment.
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
